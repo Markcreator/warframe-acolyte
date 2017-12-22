87ac402e3ae941030f0873e3b33fb9ab6cffaaca
Mark Horn <markmediahorn@hotmail.com>
2017-12-22 02:41:10 +0100

    Ful Material intergration

---
+++ js/app.js
@@ -0,0 +1,107 @@
+$(function() {
+	var worldStateURL = "http://content.warframe.com/dynamic/worldState.php";
+	var solNodeURL = "http://raw.githubusercontent.com/WFCD/warframe-worldstate-data/master/data/solNodes.json";
+	
+	var nodes;
+	var worldState;
+	
+	var acolyteNames = {
+		"StrikerAcolyte": "Angst",
+		"HeavyAcolyte": "Malice",
+		"RogueAcolyte": "Mania",
+		"AreaCasterAcolyte": "Misery",
+		"ControlAcolyte": "Torment",
+		"DuellistAcolyte": "Violence"
+	};
+	
+	getJSON(solNodeURL, function(nodeJSON) {
+		nodes = nodeJSON;
+
+		acolyteUpdate();
+		loopCountdownUpdate(30, acolyteUpdate);
+	});
+	
+	function getJSON(url, callback) {
+		$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?', function(data) {
+			callback(JSON.parse(data.contents));
+		});
+	}
+	
+	function escapeHtml(unsafe) {
+		return unsafe
+			 .replace(/&/g, "&amp;")
+			 .replace(/</g, "&lt;")
+			 .replace(/>/g, "&gt;")
+			 .replace(/"/g, "&quot;")
+			 .replace(/'/g, "&#039;");
+	}
+	
+	function loopCountdownUpdate(seconds, callback, timesLeft) {
+		if(typeof(timesLeft) == 'undefined') {
+			timesLeft = seconds;
+		}
+		var $toastContent = $('<span class="black-text">' + (timesLeft != 0 ? ('Updating in: ' + timesLeft + 's') : "Updating...") + '</span>')
+		Materialize.toast($toastContent, 1000, 'white');
+		
+		if(timesLeft == 0) {
+			callback();
+			setTimeout(function() {
+				loopCountdownUpdate(seconds, callback);
+			}, 1000);
+			
+		} else {
+			setTimeout(function() { loopCountdownUpdate(seconds, callback, timesLeft-1); }, 1000);
+		}		
+	}
+	
+	function acolyteUpdate() {
+		$("#loader").show();
+		
+		getJSON(worldStateURL, function(worldStateJSON) {
+			worldState = worldStateJSON;
+
+			$("#acolytes").empty();
+			
+			var acolyteList = worldState.PersistentEnemies;
+			for(var i = 0; i < acolyteList.length; i++) {
+				var aco = acolyteList[i];
+				
+				var acoName = aco.Icon.split("/")[aco.Icon.split("/").length-1].split(".png")[0];
+				var name = acolyteNames[acoName];
+				var disc = aco.Discovered;
+				var health = aco.HealthPercent;
+				
+				var output = [];
+				output.push('<div class="card horizontal hoverable">');
+				output.push('	<div class="card-image">');
+				output.push('		<img src="img/' + acoName + '.png">');
+				output.push('	</div>');
+				output.push('	<div class="card-stacked">');
+				output.push('		<div class="card-content flow-text">');
+				output.push("			" + name.toUpperCase() + "	<a class='right' target='_blank' href='http://warframe.wikia.com/wiki/" + name + "'>Wiki page</a>");
+				output.push('			<div class="progress grey"> <div class="determinate red" style="width: ' + (health * 100) + '%"></div> </div>');
+				output.push("			<span class='red-text'>Health: " + (health * 100).toFixed(1) + "%</span>");
+				output.push("			<br/>");
+				output.push("			Location: " + (disc ? escapeHtml(nodes[aco.LastDiscoveredLocation].value) : "Undiscovered"));
+				output.push('		</div>');
+				output.push('	</div>');
+				output.push('</div>');
+				
+				$("#acolytes").append(output.join(""));
+			}
+			
+			if(acolyteList.length == 0) {
+				var output = [];
+				output.push('<div class="card hoverable">');
+				output.push('	<div class="card-content flow-text">');
+				output.push('		No Acolytes are currently around...');
+				output.push('	</div>');
+				output.push('</div>');
+				
+				$("#acolytes").append(output.join(""));
+			}
+			
+			$("#loader").hide();
+		});
+	}
+});
