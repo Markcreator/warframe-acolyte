8cb6cbec06739e3ba029559ed3aa1d1ebfcba684
markcreator <markmediahorn@hotmail.com>
2019-01-25 22:21:17 +0100

    Added switch support

--- js/app.js
+++ js/app.js
@@ -36,7 +36,8 @@ $(function() {
 	var worldStateURLs = {
 		"pc": "http://content.warframe.com/dynamic/worldState.php",
 		"ps4": "http://content.ps4.warframe.com/dynamic/worldState.php",
-		"xbox": "http://content.xb1.warframe.com/dynamic/worldState.php"
+		"xbox": "http://content.xb1.warframe.com/dynamic/worldState.php",
+		"switch": "http://content.swi.warframe.com/dynamic/worldState.php"
 	};
 	var worldStateURL = worldStateURLs[platform];
 	var solNodeURL = "https://raw.githubusercontent.com/WFCD/warframe-worldstate-data/master/data/solNodes.json";
